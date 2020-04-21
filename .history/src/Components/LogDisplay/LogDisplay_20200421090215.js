655e177ce9911d86deef4daf31e06e4b394974bd
Derek Lamarr <derek@macbook-pro.lan>
2020-04-21 13:22:07 -0700

    css passable, attempting advanced log display by date functionality. wish me luck

---
+++ .history/src/Components/LogDisplay/LogDisplay_20200421090215.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import './LogDisplay.scss'
+import AddLog from '../AddLog/AddLog'
+
+export default class LogDisplay extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            logs: []
+        }
+        // this.getLogs = this.getLogs.bind(this)
+        this.getLogsByBaby = this.getLogsByBaby.bind(this)
+    }
+    componentDidMount(){
+        this.getLogsByBaby()
+    }
+    getLogsByBaby() {
+        const id = this.props.selectedTab
+        axios.get(`/api/logs/${id}`).then(res => {
+            this.setState({
+                logs: res.data
+            })
+        })
+    }
+    deleteLog(id) {
+        axios.delete(`/api/logs/${id}`).then(() => this.getLogs()).catch(err => console.log('Error deleting log', err))
+    }
+    render(){
+        const mappedLogs = this.state.logs.map(log => {
+            return (
+                <div className='log-display' key={log.log_id}>
+                <div>{moment(log.asleep).format('MMMM Do YYYY, h:mm:ss a')}</div>
+                <button onClick={() => this.deleteLog(log.log_id)}>Delete</button>
+                </div>
+            )
+        })
+        return(
+            <div>{mappedLogs}
+            <AddLog getLogsByBaby={this.getLogsByBaby} babyId={this.props.babyId}/>
+            </div>
+        )
+    }
+}
---
+++ .history/src/Components/Main/Main_20200421123233.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.scss'
+import BabyDisplay from '../BabyDisplay/BabyDisplay'
+import AddBaby from '../AddBaby/AddBaby'
+
+export default function Main (){
+    return(
+         <div className='main'>
+             <BabyDisplay/>
+             </div>
+    )
+}
